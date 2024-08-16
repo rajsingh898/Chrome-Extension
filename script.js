3c7363980330ba18a5359583ad28776cb7b937c0
Raj Singh <79324193+rajsingh898@users.noreply.github.com>
2024-08-16 15:17:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+fetch('https://icanhazdadjoke.com/slack')
+    .then(data => data.json())
+    .then(jokeData => {
+        const jokeText = jokeData.attachments[0].text; 
+        const jokelement=document.getElementById('jokelement');
+        jokelement.innerHTML = jokeText;
+
+
+    })
+
+    
